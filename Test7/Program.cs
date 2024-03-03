ce625f7c7101296bd711cd3e80a672f06b15d83f
neerajan rai <zlamsong6@gmail.com>
2024-03-03 10:39:54 +0545

    reworked on pre/post inc/dec and added reverse the position of string in sentence program

--- Test7/Program.cs
+++ Test7/Program.cs
@@ -9,10 +9,34 @@ namespace Test7
             Console.WriteLine(--a); //10
             Console.WriteLine(b--); //12
             Console.WriteLine(b); //11
+            Console.WriteLine(a); //10
 
             e = --c + ++d + b; //12+15+11=38
             Console.WriteLine(e); //38
             Console.WriteLine("Remember.. Now, you are a pro at pre/post incremennt/decrement");
+
+
+            int x = 10, y = 12, z;
+            x += x; //10+10=20
+            Console.WriteLine(x); //20
+
+            y = +y; //13
+            Console.WriteLine(y); //13
+
+            z = ++y + y++; //13+13=26
+            Console.WriteLine(z); //26
+
+            int p = 11, q = 12, r;
+            r = p++ + ++p; //11+13 = 24
+            Console.WriteLine(r); //24
+
+            r = q++ + ++q + ++q; //12+14+15 = 41
+            Console.WriteLine(r); //41
+
+            int s = 10, t = 11, u;
+            u = s++ + ++t + s; //10+12+11=33
+            Console.WriteLine(u); //33
+
         }
     }
 }
---
+++ Test8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Test8
+{
+    class ReverseWordOfString
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence");
+            string sentence = Console.ReadLine();
+
+            string[] stringarray = sentence.Split(' ');
+            //foreach (string str in stringarray)
+            //{
+            //    Console.WriteLine(str);
+            //}
+
+            Array.Reverse(stringarray);
+
+            //Console.WriteLine(stringarray.Length);
+            //foreach (string str1 in stringarray)
+            //{
+            //    Console.WriteLine(str1);
+            //}
+
+
+            Console.WriteLine("Reverse sentence is: ");
+            for (int i = 0; i <= stringarray.Length - 1; i++)
+            {
+                Console.Write(stringarray[i] + ' ');
+            }
+
+
+
+        }
+    }
+}
