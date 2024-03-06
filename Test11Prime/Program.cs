c9f6ce5297f8c355b9efbc3b89b75418d2514035
neerajan rai <zlamsong6@gmail.com>
2024-03-06 15:55:44 +0545

    added test 11 - prime number testing and test12 - palindrome number testing

---
+++ Test11Prime/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace IdentifyPrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int flag = 0;
+            Console.WriteLine("Enter the number to check for a prime");
+            int number = int.Parse(Console.ReadLine());
+
+            int m = number / 2;
+
+            /*Console.WriteLine("m = "+m);
+            int z = number % 2;
+            Console.WriteLine("z = " + z);*/
+
+            for (int i = 2; i <= m; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine("Number is not a prime");
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 0)
+            {
+                Console.WriteLine("Number is prime");
+            }
+        }
+    }
+}
---
+++ Test12Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, temp, r, sum = 0 ;
+
+            Console.WriteLine("Enter the number to check palindrome:");
+            number = int.Parse(Console.ReadLine());
+
+            temp = number; //temporarily holding the entered number
+
+            //logic to reverse the number
+            while(number > 0)
+            {
+                r = number % 10;
+                sum = (sum * 10) + r;
+                number = number / 10;
+            }
+            if (temp == sum) //comparing original number(currently being holded by temp field) with a reversed number
+
+                Console.WriteLine("The entered number is palindrome.");
+            else
+                Console.WriteLine("The entered number is not a palindrome");
+
+        }
+        
+    }
+}
