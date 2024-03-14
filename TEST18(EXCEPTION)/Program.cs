5a850c56fb3f61eb661da8859a2da698c300caa7
neerajan rai <zlamsong6@gmail.com>
2024-03-14 15:56:11 +0545

    added Exception

---
+++ TEST18(EXCEPTION)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace TryCatchFinally
+{
+    class Division
+    {
+        int result; 
+        Division() //CONSTRUCTOR
+        {
+            result = 0;
+        }
+        public void division(int x, int y) //METHOD
+        {
+            try
+            {
+                result = x / y; //10/0 (DIVIDED BY ZERO)
+            }         
+            catch(DivideByZeroException ex) //THIS GETS EXECUTED
+            {
+                Console.WriteLine("Exception caught: {0}", ex);
+            }
+            finally //THIS ALWAYS GETS EXECUTED REGARDLESS
+            {
+                Console.WriteLine("result: {0}", result);
+            }
+        }
+    
+    
+        static void Main(string[] args)
+        {
+            Division d = new Division();
+            d.division(10, 0);
+            int[] array = { 1, 2, 3, 4, 5 };
+            try
+            {
+
+                Console.WriteLine(array[10]); //INDEX OUT OF RANGE
+            }
+            /*
+            catch (Exception ex) //THIS CATCH CLAUSE ALREADY CATCHES ALL EXCEPTIONS OR OF A SUPER TYPE
+            {
+                Console.WriteLine("error occured!");
+            }
+            */           
+            catch (IndexOutOfRangeException ex) //THIS GETS EXECUTED
+            {
+                Console.WriteLine("index error occured!");
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("null error occured!");
+            }
+            finally //THIS ALWAYS GETS EXECUTED REGARDLESS
+            {
+                Console.WriteLine("this always gets executed regardless of the result.");
+            }
+
+        }
+    }
+}
