1cbeaadb02e47d4c7cf20f5b31b4e3c7668d282a
neerajan rai <zlamsong6@gmail.com>
2024-03-19 18:34:51 +0545

    added armstrong/narcissistic number and conversion of int to int array

---
+++ Armstrong Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+//371 = 3^3+7^3+1^3 = 27+343+1 = 371
+//1634 = 1^4+6^4+3^4+4^4 = 1634
+namespace ArmstrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, power, length, sum = 0;
+            Console.Write("enter the number you want to test : ");
+            number = int.Parse(Console.ReadLine());
+            
+            //Converting number into an integer array eg, 123 into [1, 2, 3]
+            int[] digits = number.ToString().ToCharArray().Select(x => (int)Char.GetNumericValue(x)).ToArray();
+            power = digits.Length;
+            length = power - 1;
+
+            for(int i = 0; i <= length; i++)
+            {
+                sum = sum + (int)Math.Pow(digits[i], power);
+            }
+
+            if(sum == number)
+            {
+                Console.WriteLine("Number is armstrong/narcissistic number.");
+            }
+            else
+            {
+                Console.WriteLine("Num" +
+                    "ber is not armstrong/narcissistic number.");
+            }
+            Console.ReadKey();
+       
+        }
+    }
+}
---
+++ IntIntoArray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Int_Into_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("enter the multi digit number : ");
+            number = int.Parse(Console.ReadLine());
+
+            //1. int to string
+            string digitString = number.ToString();
+            Console.WriteLine("string of {0} is {1}",number, digitString);
+
+            //2. string to char array
+            char[] digitCharArray = digitString.ToCharArray();
+            Console.WriteLine("char array of number is");
+            foreach(char c in digitCharArray)
+            {
+                Console.WriteLine(c);
+            }
+
+            //3. char array to int array
+            int[] digitIntArray = digitCharArray.Select(x => (int)Char.GetNumericValue(x)).ToArray();
+            Console.WriteLine("int array of  number is");
+            foreach (int i in digitIntArray)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(Environment.NewLine);
+
+            // in a single line of code
+            int[] intArrayOfNumber = number.ToString().ToCharArray().Select(x => (int)Char.GetNumericValue(x)).ToArray();
+            foreach(int i in intArrayOfNumber)
+            {
+                Console.WriteLine(i);
+            }
+           
+       
+
+
+            Console.ReadKey();
+        }
+    }
+}
